ae3728adf2ef284d5ee8b8bdd98d2ac66272a705
Michael Ferry <michaelferry@bluefirebusiness.com>
2025-04-19 15:33:44 -0400

    wip

--- purgecss.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    content: ["./**/*.html"], // Include all HTML files
-    css: ["./css/*.css"], // Include all CSS files
-    output: "./css/purged", // Output directory for cleaned CSS
-};
